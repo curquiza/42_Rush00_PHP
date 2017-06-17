9da95760394cb27e8d34ecacca88201f942c1cce
Sarah FRANC <sfranc@e2r5p17.42.fr>
2017-06-18 00:48:22 +0200

    bouton select non relier au back

--- all_products.php
+++ all_products.php
@@ -21,11 +21,28 @@
                     <div class="horse"><div align=center>
                         <p><?php echo $elem[2] ?></p>
                         <div style="font-style:italic"><?php echo $elem[1] ?></div>
-                        <div><br /><img src="<?php echo $elem[6] ?>"/></div><br />
-                        <div><?php echo $elem[4] ?></div>
+                        <div>
+                            <?php   
+                    if ($elem[5] == 0)
+                {
+                    echo '<p style="background-color:darkmagenta;color:white">SOLD OUT !</p>';
+                    $filigrane = 'style="opacity:0.2";';
+                }
+                    else
+                {
+                    echo "<br />";
+                    $filigrane = ""; 
+                } ?>
+                            <img <?php echo $filigrane ?> src="<?php echo $elem[6] ?>"/></div><br />
+                        <div><?php echo $elem[4] ?></div>  
                         <form action="add_to_cart.php" method="POST">
-                            <select><input type="number" name="quant" min="1" max="<?php echo $elem[5] ?>" step="1" value="0"></select>
-                            <input type="hidden" name="id" name="<?php echo $elem[5] ?>">
+                            
+                            <select><input<?php 
+                    for ($i=0; $i <= $elem[5]; $i++)
+                        echo "<option value =".$i.">".$i."</option>"; ?>></select>
+                                
+<!--                                <input type="number" name="quant" min="1" max="<?php echo $elem[5] ?>" step="1" value="0">-->
+                            <input type="hidden" name="id" name="<?php echo $elem[0] ?>">
                             <input class="button_buy" type="submit" name="<?php echo $elem[0] ?>" value="I want it !">
                         </form><br /></div>
                     </div>
