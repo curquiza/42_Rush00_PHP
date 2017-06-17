05d25a5f9e6f18ebd10854c82a6c6f6e02b308a1
Clementine URQUIZAR <curquiza@e2r5p18.42.fr>
2017-06-17 14:39:15 +0200

    sign in intergré

--- sign_in.php
+++ sign_in.php
@@ -1,30 +1,18 @@
-<?php
-session_start();
-           
-function ft_auth($login, $passwd)
-{
-    if ($login === NULL || $passwd === NULL || file_exists("private/passwd") === FALSE)
-        return (FALSE);
-    $list = unserialize(file_get_contents("private/passwd"));
-    foreach($list as $elem)
-    {
-        if ($elem['login'] === $login && $elem['passwd'] === hash("whirlpool", $passwd))
-            return (TRUE);
-    }
-    return (FALSE);
-}
-
-if (ft_auth($_POST['login'], $_POST['passwd']) === TRUE)
-{
-    $_SESSION['logged_on_user'] = $_POST['login'];
-    echo "Welcome back ".$_POST['login']." !\n";
-    echo '<html><body><br /><a href="index.php">Let\'s go shopping !</a></body></html>';
-}
-else
-{
-    $_SESSION['logged_on_user'] = "";
-    echo "Sorry, we were enable to log you in...\n";
-    echo '<html><body><br /><a href="index.php">Back to the main page</a></body></html>';
-}
-print_r($_SESSION);
-?>
+<html>
+    <head>
+        <?php include("settings.php"); ?>
+    </head>
+    <body>
+    <?php include("header.php"); ?>
+    <H1>Sign in</H1>
+    <form action="sign_in_action.php" method="POST">
+        Login: <input type="text" name="login" value="">
+        <br/ >
+        Password: <input type="password" name="passwd" value="">
+        <br/ >
+        <input type="submit" name="submit" value="OK">
+    </form>
+    <a href="sign_up.html">Not a client ? Sign up here !</a>
+    <?php include("footer.php"); ?>
+    </body>
+</html>
