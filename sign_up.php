592bf6151829dfb6b40beba22bb56e905fc665ca
Clementine URQUIZAR <curquiza@e2r5p18.42.fr>
2017-06-17 15:49:32 +0200

    sign up fini

--- sign_up.php
+++ sign_up.php
@@ -1,16 +1,18 @@
 <html>
     <head>
-        <meta charset="UTF-8">
+      <?php include("settings.php"); ?>
     </head>
     <body>
-    <H1>Sign Up</H1>
+    <?php include("header.php"); ?>
+    <H1>Sign Up</H1> <br />
     <form action="sign_up_action.php" method="POST">
         Choose a login: <input type="text" name="login" value="">
         <br/ >
         Create a password: <input type="password" name="passwd" value="">
         <br/ >
         <input type="submit" name="submit" value="OK">
-    </form>
-        <a href="sign_in.html">Already a client ? Sign in here !</a>
+    </form> <br />
+    <a href="sign_in.php">Already a client ? Sign in here !</a>
+    <?php include("footer.php"); ?>
     </body>
 </html>
--- sign_up_action.php
+++ sign_up_action.php
@@ -1,6 +1,24 @@
 <?php
 session_start();
+?>
 
+<html>
+    <head>
+      <?php include("settings.php"); ?>
+    </head>
+    <body>
+    <?php include("header.php"); ?>
+    <H1>Sign Up</H1> <br />
+    <form action="sign_up_action.php" method="POST">
+        Choose a login: <input type="text" name="login" value="">
+        <br/ >
+        Create a password: <input type="password" name="passwd" value="">
+        <br/ >
+        <input type="submit" name="submit" value="OK">
+    </form> <br />
+    
+
+<?php
 function    ft_check_login($list, $new_login)
 {
     foreach($list as $elem)
@@ -33,7 +51,7 @@ function    ft_check_login($list, $new_login)
                 // transvaser le panier en cours ! Et le merger au besoin avec le panier deja rempli !
                 $new_user = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd']), "cart" => NULL, "role" => 0);
                 $list[] = $new_user;
-                print_r($list);
+                //print_r($list);
                 $data = serialize($list);
                 file_put_contents("private/passwd", $data."\n");
                 $_SESSION['logged_on_user'] = $_POST['login'];
@@ -46,5 +64,9 @@ function    ft_check_login($list, $new_login)
     }
     else
         echo "Enable to create your account... Please, Try again !\n";
-print_r($_SESSION);
+//print_r($_SESSION);
 ?>
+        
+        <?php include("footer.php"); ?>
+    </body>
+</html>
