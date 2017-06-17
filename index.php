e80c1b790145d3d8e3c507f2276d0eaa532b6b81
Sarah FRANC <sfranc@e2r5p17.42.fr>
2017-06-18 00:10:58 +0200

    avec la bdd c'est mieux

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
 <body>
     <?php include("header.php"); ?>
     <h2> PAGE D'ACCUEIL </h2>
+    <IMG align=center src="http://howrseinfo.com/img/howrse.png"></IMG>
     <?php include("footer.php"); ?>
 </body>
 </html>
