d7a59026f0bfd58a52bfa06eec5cf0c2e5319710
Sarah FRANC <sfranc@e2r5p17.42.fr>
2017-06-17 17:22:19 +0200

    pages produits avec bouton I want one

--- legendary.php
+++ legendary.php
@@ -5,21 +5,26 @@
     <body>
     <?php include("header.php"); ?>
     <H2>Legendaries</H2>
-        <div style="display:flex">
+        <div align=center><div class="product">
         <?php
         $fd = fopen("bdd/horses.csv", "r");
         while (($line = fgetcsv($fd, "r")) !== FALSE)
             $data[] = $line;
-//        print_r($data);
         foreach($data as $elem)
         {
-            if (strstr($elem[1], "Legendary") !== FALSE)
-            {
-                echo '<div>'.$elem[2].'<br /><img src="'.$elem[6].'"/></div>';
-            }
+            if (strstr($elem[1], "Legendary") !== FALSE) : ?>
+                <div class="horse"><div align=center>
+                    <p><?php echo $elem[2] ?></p>
+                    <div><br /><img src="<?php echo $elem[6] ?>"/></div><br />
+                    <div><?php echo $elem[4] ?> $</div>
+                    <form action="add_to_cart.php">
+                        <input class="button_buy" type="button" name="<?php echo $elem[0] ?>" value="I want one !">
+                    </form><br /></div>
+                </div>
+            <?php endif;
         }
         ?>
-        </div>
+        </div></div>
     <?php include("footer.php"); ?>
     </body>
 </html>
