92193bbba21366fe1c62398d4f1e3425fb616e38
Clementine URQUIZAR <curquiza@e2r5p18.42.fr>
2017-06-18 22:57:33 +0200

    install fini

--- install.php
+++ install.php
@@ -25,20 +25,20 @@ $tab[] = array('1','Horse','Mustang','bay','4999','15','https://i.skyrock.net/24
 $tab[] = array('2','Horse','Pure-Bred Spanish Horse','grey','4999','19','https://www.equideow.com/media/equideo/image/chevaux/adulte/iberique/normal/300/gr-pml.png');
 $tab[] = array('17','Poney','Fjord','isabel','1799','10','https://www.equideow.com/media/equideo/image/chevaux/adulte/fjord/normal/300/bbk.png');
 $tab[] = array('3','Horse','French Trotter','bay','3999','26','https://i.skyrock.net/2403/75432403/pics/2943165259_1_5.png');
-$tab[] = array('10','"Legendary, Horse"','Unicorn','white','500000','1','https://i.skyrock.net/6802/79146802/pics/3013556331_1_21_vMvQTm2E.png');
+$tab[] = array('10','Legendary, Horse','Unicorn','white','500000','1','https://i.skyrock.net/6802/79146802/pics/3013556331_1_21_vMvQTm2E.png');
 $tab[] = array('4','Horse','Pure-Bred Arab Horse','white','3999','15','https://i.skyrock.net/2403/75432403/pics/2942892871_1_5.png');
 $tab[] = array('5','Horse','Marwari','painted black','9999','7','https://ouranos.equideow.com/media/equideo/image/chevaux/adulte/marwari/normal/300/pie-ov-nr.png');
 $tab[] = array('6','Poney','Shetland','alezan','999','50','https://www.equideow.com/media/equideo/image/chevaux/adulte/shetland/normal/300/alz.png');
 $tab[] = array('7','Poney','Connemara','bay','1599','30','https://www.equideow.com/media/equideo/image/chevaux/adulte/poney-primitif/normal/300/bai-b.png');
-$tab[] = array('8','"Foal, Poney"','Shetland','alezan','499','2','https://ouranos.equideow.com/media/equideo/image/chevaux/poulain/shetland/normal/300/alz.png');
-$tab[] = array('9','"Foal, Horse"','Mustang','painted','899','2','https://ouranos.equideow.com/media/equideo/image/chevaux/poulain/generique1/normal/300/bkt-alz-b.png');
-$tab[] = array('11','"Legendary, Horse"','Pegasus','black','500000','1','https://ouranos.equideow.com/media/equideo/image/chevaux/adulte/frison/pegase/300/nr.png');
+$tab[] = array('8','Foal, Poney','Shetland','alezan','499','2','https://ouranos.equideow.com/media/equideo/image/chevaux/poulain/shetland/normal/300/alz.png');
+$tab[] = array('9','Foal, Horse','Mustang','painted','899','2','https://ouranos.equideow.com/media/equideo/image/chevaux/poulain/generique1/normal/300/bkt-alz-b.png');
+$tab[] = array('11','Legendary, Horse','Pegasus','black','500000','1','https://ouranos.equideow.com/media/equideo/image/chevaux/adulte/frison/pegase/300/nr.png');
 $tab[] = array('12','Horse','Camargue','white','2999','10','https://www.equideow.com/media/equideo/image/chevaux/adulte/primitif/normal/300/gr-c.png');
-$tab[] = array('13','"Legendary, Horse"','Big Pegasus','white','500000','1','https://ouranos.preprod.equideow.com/media/equideo/image/chevaux/adulte/shire/pegase/300/gr-c.png?1828806360');
-$tab[] = array('14','Poney','Dartmoor','"brown',' black"','1199','50','https://www.equideow.com/media/equideo/image/chevaux/adulte/poney-leger/normal/300/nr.png');
-$tab[] = array('15','"Legendary, Horse"','Old Unicorn','white','500000','0','https://ouranos.equideow.com/media/equideo/image/chevaux/adulte/poney-sport/licorne/300/gr-t.png');
-$tab[] = array('16','"Foal, Poney"','Fjord','isabel','499','50','https://i.skyrock.net/2403/75432403/pics/2942905635_1_3.png');
-$tab[] = array('18','"Legendary, Poney"','8-leg Fire Poney','fire','500000','1','https://s-media-cache-ak0.pinimg.com/736x/da/4b/7a/da4b7aa3609ef0d096b5b504af6352a4.jpg');
+$tab[] = array('13','Legendary, Horse','Big Pegasus','white','500000','1','https://ouranos.preprod.equideow.com/media/equideo/image/chevaux/adulte/shire/pegase/300/gr-c.png?1828806360');
+$tab[] = array('14','Poney','Dartmoor',' black','1199','50','https://www.equideow.com/media/equideo/image/chevaux/adulte/poney-leger/normal/300/nr.png');
+$tab[] = array('15','Legendary, Horse','Old Unicorn','white','500000','0','https://ouranos.equideow.com/media/equideo/image/chevaux/adulte/poney-sport/licorne/300/gr-t.png');
+$tab[] = array('16','Foal, Poney','Fjord','isabel','499','50','https://i.skyrock.net/2403/75432403/pics/2942905635_1_3.png');
+$tab[] = array('18','Legendary, Poney','8-leg Fire Poney','fire','500000','1','https://s-media-cache-ak0.pinimg.com/736x/da/4b/7a/da4b7aa3609ef0d096b5b504af6352a4.jpg');
 
 ft_putcsv($tab, "bdd/product.csv");
 
