4133ac58355d4a65b97013486ab3f2b7c0128e80
Clementine URQUIZAR <curquiza@e2r5p18.42.fr>
2017-06-18 16:44:04 +0200

    admin user fini

--- admin_users_action.php
+++ admin_users_action.php
@@ -17,6 +17,7 @@ $tab = unserialize($file);
 
 $_SESSION['flag_users_remove'] = 0;
 $_SESSION['flag_users_add'] = 0;
+$_SESSION['flag_users_change'] = 0;
 
 /* REMOVED */
 if ($_POST['submit'] == "Remove" && $_POST['login'] != NULL)
@@ -40,7 +41,7 @@ if ($_POST['submit'] == "Remove" && $_POST['login'] != NULL)
 /* ADDED */
 else if ($_POST['submit'] == "Add" && $_POST['login'] != NULL && ($_POST['role'] == "Yes" || $_POST['role'] == 'No'))
 {
-	if (($index = ft_get_index($_POST['login'], $tab)) == -1)
+	if (ft_get_index($_POST['login'], $tab) == -1)
 	{
 		if ($_POST['role'] == 'Yes')
 			$role = 1;
@@ -56,6 +57,42 @@ else if ($_POST['submit'] == "Add" && $_POST['login'] != NULL && ($_POST['role']
 }
 else if ($_POST['submit'] == "Add")
 		$_SESSION['flag_users_add'] = -2;
+
+/* CHANGE */
+else if ($_POST['submit'] == "Change" && $_POST['login'] != NULL && ($_POST['role'] == "Yes" || $_POST['role'] == 'No'))
+{
+	if (($index = ft_get_index($_POST['login'], $tab)) != -1)
+	{
+		if ($_POST['login'] == "admin")
+			$_SESSION['flag_users_change'] = -3;
+		else
+		{
+			if ($_POST['newlogin'] != NULL && ft_get_index($_POST['newlogin'], $tab) == -1)
+			{
+				$tab[$index]['login'] = $_POST['newlogin'];
+				if ($_POST['login'] == $_SESSION['logged_on_user'])
+					$_SESSION['logged_on_user'] = $_POST['newlogin'];
+			}
+			if ($_POST['login'] == $_SESSION['logged_on_user'] && $_POST['role'] == 'No')
+				$_SESSION['flag_users_change'] = -4;
+			else
+			{
+				if ($_POST['role'] == 'Yes')
+					$role = 1;
+				else
+					$role = 0;
+				$tab[$index]['role'] = $role;
+				$file = serialize($tab);
+				file_put_contents("private/passwd", $file);
+				$_SESSION['flag_users_change'] = 1;
+			}
+		}
+	}
+	else
+		$_SESSION['flag_users_change'] = -1;
+}
+else if ($_POST['submit'] == "Change")
+		$_SESSION['flag_users_change'] = -2;
 
 header("Location: admin_users_page.php");
 
--- admin_users_page.php
+++ admin_users_page.php
@@ -16,11 +16,13 @@ session_start();
 		{
 			?>
 			<form class="form-admin" action="admin_users_action.php" method="POST">
-				Login: <input type="text" name="login" value="<?php echo $elem['login']; ?>">
+				Login: <?php echo $elem['login']; ?> <br />
+				<input type="hidden" name="login" value="<?php echo $elem['login']; ?>">
+				New login: <input type="text" name="newlogin" value="">
 				Admin: <input type="text" name="role" value="<?php if ($elem['role'] == 1) echo 'Yes'; else echo 'No';?>">
 				<input type="submit" name="submit" value="Change">
 				<input type="submit" name="submit" value="Remove">
-			</form>
+			</form> <br />
 			<?php
 		}
 		?>
@@ -35,38 +37,61 @@ session_start();
 		/* Erreurs pour le removed */
 		if ($_SESSION['flag_users_remove'] == -1)
 		{
-			?> <br/> <p>Impossible to remove : this account does not exist </p> <?php
+			?> <br /> <p>Impossible to remove : this account does not exist </p> <?php
 		}
 		else if ($_SESSION['flag_users_remove'] == -2)
 		{
-			?> </br> <p>Impossible to remove your own account </p> <?php
+			?> <br /> <p>Impossible to remove your own account </p> <?php
 		}
 		else if ($_SESSION['flag_users_remove'] == -3)
 		{
-			?>  <br/> <p>Impossible to remove the "admin" account </p> <?php
+			?>  <br /> <p>Impossible to remove the "admin" account </p> <?php
 		}
 		else if ($_SESSION['flag_users_remove'] == 1)
 		{
-			?> </br> <p> Successfuly removed </p> <?php
+			?> <br /> <p> Successfuly removed </p> <?php
 		}
 
-		/* Erreur pour le add */
+		/* Erreurs pour le add */
 		if ($_SESSION['flag_users_add'] == -1)
 		{
 			?> <br/> <p>Impossible to add : this account already exists </p> <?php
 		}
 		else if ($_SESSION['flag_users_add'] == -2)
 		{
-			?> <br/> <p>Impossible to add : you must choose a valid name and a valid admin status (Yes or No)</p> <?php
+			?> <br/> <p>Impossible to add : you must choose a valid login and a valid admin status (Yes or No)</p> <?php
 		}
 		else if ($_SESSION['flag_users_add'] == 1)
 		{
 			?> </br> <p> Successfuly added </p> <?php
 		}
 
+		/*Erreurs pour le change */
+		if ($_SESSION['flag_users_change'] == -1)
+		{
+			?> <br/> <p>Impossible to modify : this login is already used </p> <?php
+		}
+		else if ($_SESSION['flag_users_change'] == -2)
+		{
+			?> <br/> <p>Impossible to modify : you must choose a valid new login or a new valid admin status (Yes or No)</p> <?php
+		}
+		else if ($_SESSION['flag_users_change'] == -3)
+		{
+			?>  <br /> <p>Impossible to modify the "admin" account </p> <?php
+		}
+		else if ($_SESSION['flag_users_change'] == -4)
+		{
+			?>  <br /> <p>Impossible to demote yourself </p> <?php
+		}
+		else if ($_SESSION['flag_users_change'] == 1)
+		{
+			?> </br> <p> Successfuly modify </p> <?php
+		}
+
 		/* Re-initialisation des flags */
 		$_SESSION['flag_users_remove'] = 0;
 		$_SESSION['flag_users_add'] = 0;
+		$_SESSION['flag_users_change'] = 0;
 		?>
 		<br />
     	<?php include("footer.php"); ?>
